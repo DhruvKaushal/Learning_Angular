a34e3c396a3c9efcce4421434d912c6bac045021
DhruvKaushal <dhruvkaushal1997@gmail.com>
2020-08-02 14:59:24 +0530

    filters

--- angular-js/app/app.js
+++ angular-js/app/app.js
@@ -1,5 +1,26 @@
 var myApp = angular.module('myApp', []);
 console.log("hello")
-myApp.controller('myAppController', function($scope){
-    $scope.message = 'Hello world its dhruv'
-})
+myApp.controller('myAppController', ['$scope', function($scope){
+    $scope.ninjas = [
+        {
+            name: 'yoshi',
+            belt: 'green',
+            rate: 30
+        },
+        {
+            name: 'crystal',
+            belt: 'yellow',
+            rate: 20
+        },
+        {
+            name: 'ryu',
+            belt: 'orange',
+            rate: 10
+        },
+        {
+            name: 'shaun',
+            belt: 'black',
+            rate: 500
+        }
+    ]
+}])
